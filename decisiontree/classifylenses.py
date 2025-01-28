7651a6e79ca1b3b66dbc7f4832f933916e822853
YSL05 <271367835@qq.com>
2025-01-28 14:02:54 +0800

    practice the decision tree

--- decisiontree/classifylenses.py
+++ decisiontree/classifylenses.py
@@ -1,8 +1,11 @@
 import trees
+import decisionTreePlot
 
 fr = open('lenses.txt')
 
 lenses = [inst.strip().split('\t') for inst in fr.readlines()]
 lenseslabels = ['age', 'prescript', 'astigmatic', 'tearRate']
 lensestree = trees.createtree(lenses, lenseslabels)
+
 print(lensestree)
+decisionTreePlot.createPlot(lensestree)
