908137f353b9057d800cf3bea006a9457cce656e
YSL05 <271367835@qq.com>
2025-02-04 20:09:58 +0800

    impl bayes classify

--- naivebayes/bayes.py
+++ naivebayes/bayes.py
@@ -2,10 +2,10 @@
 import numpy as np
 from math import *
 def loaddata():
-    dataset = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'], #[0,0,1,1,1......]
+    dataset = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
                    ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
-                   ['stop', 'posting', 'stupid', 'worthless', 'gar e'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
                    ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
                    ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
     labels = [0, 1, 0, 1, 0, 1]
@@ -32,28 +32,41 @@ def trainNB0(dataset, labels):
     pAbusive = sum(labels) / float(datasetsize)
     p0num = np.ones(vocabsize)
     p1num = np.ones(vocabsize)
-    p0denom = 2.0
-    p1denom = 2.0
     for i in range(datasetsize):
         if labels[i] == 1:
             p1num = p1num + dataset[i]
-            p1denom = p1denom + sum(dataset[i])
         else:
             p0num = p0num + dataset[i]
-            p0denom = p0denom + sum(dataset[i])
-    p0vect = p0num / p0denom
-    p1vect = p1num / p1denom
+    p0vect = [log(i / sum(p0num)) for i in p0num]
+    p1vect = [log(i / sum(p0num)) for i in p1num]
     return p0vect, p1vect, pAbusive
 
-if __name__ == "__main__":
+def classifyNB(vect, p0vect, p1vect, pAbusive):
+    p1 = sum(vect * p1vect) + log(pAbusive)
+    p0 = sum(vect * p0vect) + log(1.0 - pAbusive)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    # get training dataset
     dataset, labels = loaddata()
     vocabset = createvocablist(dataset)
     datamatrix = []
     for data in dataset:
         datamatrix.append(setofworld2vec(vocabset, data))
+    # training the modle
     p0v, p1v, pab = trainNB0(datamatrix, labels)
-    print(vocabset)
-    print(p0v)
-    print(p1v)
-    print(pab)
+
+    # test the modle
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = np.array(setofworld2vec(vocabset, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0v, p1v, pab))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = np.array(setofworld2vec(vocabset, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0v, p1v, pab))     
+
+if __name__ == "__main__":
+    testingNB()
     
